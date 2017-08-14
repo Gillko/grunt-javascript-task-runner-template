ee78a3960aeddd6e3c5424444518fe11143d1856
Gilko <gilles_vanpeteghem@hotmail.com>
2017-08-14 18:09:59 +0200

    15th Commit - adding script-links task to change the path to the links and script tags in the export folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,37 +12,83 @@ module.exports = function(grunt){
 		sass: {
 			dist: {
 				files: {
-					'export/assets/css/main.css': 'assets/sass/main.scss'
+					'src/assets/css/main.css': 'src/assets/sass/main.scss'
 				}
 			}
 		},
 		cssmin: {
-		  target: {
-			files: [{
-				expand: true,
-				cwd: 'export/assets/css',
-				src: ['*.css', '!*.min.css'],
-				dest: 'export/assets/css',
-				ext: '.min.css'
-			}]
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'src/assets/css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'export/assets/css',
+					ext: '.min.css'
+				}]
 			}
 		},
 		uglify: {
 			my_target: {
 				files: {
-					'export/assets/js/main.min.js': 'assets/js/main.js'
+					'export/assets/js/main.min.js': 'src/assets/js/main.js'
 				}
 			}
 		},
 		includes: {
 			build: {
-				cwd: 'site',
+				cwd: 'src',
 				src: ['*.html'],
 				dest: 'export',
 				options: {
 					flatten: true,
-					includePath: 'assets/includes'
+					includePath: 'src/includes'
 				}
+			}
+		},
+		tags: {
+			buildScriptsSrc: {
+				options: {
+					scriptTemplate: '<script type="text/javascript" src="assets/js/main.js"></script>',
+					openTag: '<!-- start script template tags -->',
+					closeTag: '<!-- end script template tags -->'
+				},
+				src: [
+					'src/assets/js/*.js'
+				],
+				dest: 'src/index.html'
+			},
+			buildLinksSrc: {
+				options: {
+					linkTemplate: '<link rel="stylesheet" type="text/css" href="assets/css/main.css">',
+					openTag: '<!-- start css template tags -->',
+					closeTag: '<!-- end css template tags -->'
+				},
+				src: [
+					'src/assets/css/*.css'
+				],
+				dest: 'src/index.html'
+			},
+			buildScriptsExport: {
+				options: {
+					scriptTemplate: '<script type="text/javascript" src="assets/js/main.min.js"></script>',
+					openTag: '<!-- start script template tags -->',
+					closeTag: '<!-- end script template tags -->'
+				},
+				src: [
+					'export/assets/js/*.js'
+				],
+				dest: 'export/index.html'
+			},
+			buildLinksSrc: {
+				options: {
+					linkTemplate: '<link rel="stylesheet" type="text/css" href="assets/css/main.min.css">',
+					openTag: '<!-- start css template tags -->',
+					closeTag: '<!-- end css template tags -->'
+				},
+				src: [
+					'export/assets/css/*.css'
+				],
+				dest: 'export/index.html'
 			}
 		},
 		connect: {
@@ -50,7 +96,7 @@ module.exports = function(grunt){
 				options: {
 					hostname: "localhost",
 					port: 3000,
-					base: 'site/',
+					base: 'src/',
 					livereload: true
 				}
 			}
@@ -60,15 +106,15 @@ module.exports = function(grunt){
 				livereload: true,
 			},
 			css: {
-				files: ['assets/sass/*.scss'],
+				files: ['src/assets/sass/*.scss'],
 				tasks: ['sass']
 			},
 			js: {
-				files: ['assets/js/*.js'],
+				files: ['src/assets/js/*.js'],
 				tasks: ['uglify']
 			},
 			html: {
-				files: ['site/index.html'],
+				files: ['src/index.html'],
 			}
 		}
 	});
@@ -76,7 +122,8 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-includes');
+	grunt.loadNpmTasks('grunt-script-link-tags');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'includes', 'connect', 'watch']);
+	grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'includes', 'tags', 'connect', 'watch']);
 };
