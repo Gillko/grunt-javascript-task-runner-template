9bb327d3cff2a0a088f96c8bf47b8d3e402e9d4d
Gilko <gilles_vanpeteghem@hotmail.com>
2017-08-14 16:25:34 +0200

    12th Commit - task for auto refreshing the browser and live reload the changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,20 @@ module.exports = function(grunt){
 				}
 			}
 		},
+		connect: {
+			server: {
+				options: {
+					hostname: "localhost",
+					port: 3000,
+					base: 'site/',
+					livereload: true
+				}
+			}
+		},
 		watch: {
+			options: {
+				livereload: true,
+			},
 			css: {
 				files: ['assets/sass/*.scss'],
 				tasks: ['sass']
@@ -53,13 +66,17 @@ module.exports = function(grunt){
 			js: {
 				files: ['assets/js/*.js'],
 				tasks: ['uglify']
+			},
+			html: {
+				files: ['site/index.html'],
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-includes');
-	grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'includes']);
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'includes', 'connect', 'watch']);
 };
